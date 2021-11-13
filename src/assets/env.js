0531eb724deff72549207ee5f2b3edbe4b4f063c
crsmok <museum.grodno@gmail.com>
2021-11-13 15:57:25 +0200

    DashBoard

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,8 +2,8 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = 'https://museums.belhost.by/api/';
-
+  //window.__env.apiUrl = 'https://museums.belhost.by/api/';
+  window.__env.apiUrl = 'http://127.0.0.1:8000/api/';
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
   window.__env.enableDebug = true;
